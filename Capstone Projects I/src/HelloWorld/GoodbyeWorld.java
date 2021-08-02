2ccc517f28ae1338bae1537b3f17a50022b91fce
lehlohonolo <lehlohonolo@lehlohonolo-HP-EliteBook-2540p>
2021-08-02 20:19:53 +0200

    Second Commit

---
+++ Capstone Projects I/src/HelloWorld/GoodbyeWorld.java
@@ -0,0 +1,11 @@
+package HelloWorld;
+
+public class GoodbyeWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Goodbye World");
+
+	}
+
+}
